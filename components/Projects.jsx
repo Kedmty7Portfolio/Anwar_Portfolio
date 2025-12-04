084722b2ce72a1230354d057608bb73b4ab495dd
Khedmity <kedmty_portfolio@hotmail.com>
2025-12-04 11:18:46 +0200

    up

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,5 +1,6 @@
+import Image from "next/image";
 const projects = [
-  {
+  {img:"",
     title: "كتابة محتوى حساب سامية السناني",
     description: `تحس أنك رجعت للدنيا… لكن مو نفس الناس؟
 ترجع للأماكن القديمة… وما يعود لك نفس الشعور؟
@@ -9,7 +10,7 @@ const projects = [
     end_d: "اليوم ",
     link: "https://x.com/samiyahalsanai/status/1995319896448659646?s=12",
   },
-  {
+  {img:"inst.png",
     title: "عيناي",
     description: `
     مشروع وطني طور محليا؛ لاستخدام تقنيات الذكاء
@@ -38,8 +39,11 @@ export default function Projects() {
         {projects.map((p, idx) => (
           <div
             key={idx}
-            className={`flex-1/2 p-5 rounded-xl h-[280px] bg-[#cdccca40]`}
+            className={`flex-1/2 p-5 rounded-xl h-[480px] bg-[#cdccca40]`}
           >
+            <div className={`h-53 w-full rounded-lg relative overflow-hidden`}>
+   <Image src={`/${p.img}`} alt={`${p.title}`} fill className={`object-fit`}/>
+            </div>
             <h2 className={`text-lg font-bold leading-12 mb-2`}>{p.title}</h2>
             <p>{p.description}</p>
             <span className="flex items-center gap-5 my-3">
